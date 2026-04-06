1b582f0edafbeb9a5855449941f3ef4bfd712e44
art-bot5000 <pete@artbot5000.com>
2026-04-06 22:35:19 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── INCREMENT THIS VERSION NUMBER EVERY TIME YOU DEPLOY ──────
-const CACHE_VERSION = 'stockroom-v20';
+const CACHE_VERSION = 'stockroom-v21';
 const CACHE_NAME    = CACHE_VERSION;
 
 const CACHE_URLS = [
